3f73378cfa40d4ddcd8a90d2dad372c1c1bd2b02
kevinterwilliger <kevint3@illinois.edu>
2018-10-16 17:16:18 -0500

    added writes, still overwrites existing

--- sheetEditor.py
+++ sheetEditor.py
@@ -1,9 +1,12 @@
 from googleapiclient.discovery import build
 from httplib2 import Http
 from oauth2client import file, client, tools
+import sys
 
-global SCOPES
+global SCOPES,url,WRITE_FILE_ID
 SCOPES = "https://www.googleapis.com/auth/spreadsheets"
+WRITE_FILE_ID = '1Kw5QL1EjtCS2ugMKpahZdFQM2zmDj1-O6SQ6col8rpU'
+url = sys.argv[1]
 
 def oauth_verify() :
     store = file.Storage('token.json')
@@ -16,28 +19,59 @@ def oauth_verify() :
     service = build('sheets', 'v4', http=creds.authorize(Http()))
     return service
 
-def getEmails(auth) :
-    ATTENDENCE_SHEET_ID = "1xhMQ6JUQnof1dvfPpA3KXqq1FFp3YKH5t06-pwfaT78"
-    result = auth.spreadsheets().values().get(spreadsheetId=ATTENDENCE_SHEET_ID,range="B2:B70").execute()
-    values = result.get('values', [])
-    return values
+def getTitle(auth,id) :
+    result = auth.spreadsheets().get(spreadsheetId=id).execute()
+    title = result.get('properties').get('title')
+    r = auth.spreadsheets().values().get(spreadsheetId=id,range="A1:A").execute()
+    dates = r.get('values',[])
+    title = title + " " + dates[1][0]
+    return title
 
-def getNames(auth) :
-    ATTENDENCE_SHEET_ID = "1xhMQ6JUQnof1dvfPpA3KXqq1FFp3YKH5t06-pwfaT78"
-    result = auth.spreadsheets().values().get(spreadsheetId=ATTENDENCE_SHEET_ID,range="C2:C70").execute()
-    values = result.get('values', [])
-    return values
+def getEmails(auth,id) :
+    result = auth.spreadsheets().values().get(spreadsheetId=id,range="B2:B").execute()
+    return result
 
-def writeBatch(auth) :
-    WRITE_FILE_ID = "1Kw5QL1EjtCS2ugMKpahZdFQM2zmDj1-O6SQ6col8rpU"
-    result = auth.spreadsheets().values().batchUpdate(spreadsheetId=WRITE_FILE_ID, range)
-    
+def getNames(auth,id) :
+    result = auth.spreadsheets().values().get(spreadsheetId=id,range="C2:C").execute()
+    return result
+
+def writeAttendees(auth,name_range,email_range) :
+    names = name_range.get('values',[])
+    emails = email_range.get('values',[])
+    body_names = {'values' : names }
+    name_result = auth.spreadsheets().values().update(spreadsheetId=WRITE_FILE_ID,
+                                                      range='B2:B',
+                                                      valueInputOption='RAW',
+                                                      body=body_names).execute()
+
+    body_emails = {'values' : emails }
+    email_result = auth.spreadsheets().values().update(spreadsheetId=WRITE_FILE_ID,
+                                                       range='C2:C',
+                                                       valueInputOption='RAW',
+                                                       body=body_emails).execute()
+def updateAttendance(auth,event) :
+    value_range = auth.spreadsheets().values().get(spreadsheetId=WRITE_FILE_ID,range='B2:Z').execute()
+    values = value_range.get('values',[])
+    for person in values :
+        person.append(event)
+    body = {'values': values}
+    result = auth.spreadsheets().values().update(spreadsheetId=WRITE_FILE_ID,range='B2:Z',valueInputOption='RAW',body=body).execute()
+
+def getId(url) :
+    start = url.find('d/')
+    end = url.find('/edit')
+    id = url[start+2:end]
+    return id
 
 def main() :
+    id = getId(url)
     auth = oauth_verify()
-    emails = getEmails(auth)
-    names = getNames(auth)
-
+    title = getTitle(auth,id)
+    email_range = getEmails(auth,id)
+    name_range = getNames(auth,id)
+    writeAttendees(auth,name_range,email_range)
+    updateAttendance(auth,title)
+    print("done")
 
 if __name__ == "__main__" :
     main()
